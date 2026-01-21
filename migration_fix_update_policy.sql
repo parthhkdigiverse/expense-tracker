d84c5302e051d506eb30a64ca4bf8fbb1613dc89
parthhkdigiverse <parthhkdigiverse@gmail.com>
2026-01-21 14:53:27 +0530

    Delete migration_fix_update_policy.sql

--- migration_fix_update_policy.sql
+++
@@ -1,3 +0,0 @@
--- Run this in Supabase SQL Editor to allow editing bank accounts
-create policy "Users can update their own bank accounts." on public.bank_accounts
-  for update using (auth.uid() = user_id);
